14705555ee08fa35af8568ddab9a456d462bbe5d
Alexey Ascheulov <alexey.ashcheulov@gmail.com>
2020-04-18 12:17:17 +0300

    add task1 description

--- src/main/java/com/sberbank/task1/Main.java
+++ src/main/java/com/sberbank/task1/Main.java
@@ -2,6 +2,10 @@ package com.sberbank.task1;
 
 import java.util.Random;
 
+/*
+Напишите программу для сортировки массива, использующую метод пузырька
+*/
+
 public class Main {
 
     public static void main(String[] args) {
